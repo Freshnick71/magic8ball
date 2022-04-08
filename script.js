f53bcce47afaef746e114b056284876250e05c8b
Bishop Oparaugo <bco7480@g.rit.edu>
2022-04-07 20:26:21 -0400

    Included a user input text field to get the name and answer the question, also updated the readme file.

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 let answerButton = document.querySelector("#getAnswer")
 let magicPic = document.querySelector("#magic-pic")
+let answers = ["Yes, ", "No, ", "Maybe, ", "Not clear!", "Ask again later, ", "No way!", "The likelihood is in your favor ", "Unlikely ", "Ask your mom, ", "Sounds like an issue not an issMe, ", "Get fucked!", "Ask Eboard on slack ", "Ask AvanBot ", "Don't give up!"]
+let output = document.querySelector("#output")
+let random = Math.floor(Math.random() * answers.length)
 
-let answers =["Yes!","No!","Maybe!","Not clear!","Ask again later!","No way!","The likelihood is in your favor!", "unlikely!","Ask your mom!", "Sounds like an issue not an issMe", "Get fucked!", "Ask Eboard on slack", "Ask AvanBot", "Don't give up!"]
+function func() {
+    let name = prompt("Enter your name:", " Csher");
+    let question = prompt("What is it you desire:", "Is there an afterlife");
+    let response;
+    if (name == null || name == "" && question == null || question == "") {
+        response = "Prompt was canceled due to an error.";
+    } else {
+        response = answers[random] + " " + name;
+    }
+    output.innerHTML = response;
+}
 
-answerButton.addEventListener("click",()=>{
-  magicPic.classList.toggle("wiggle")
-  setTimeout(()=>{
+answerButton.addEventListener("click", () => {
     magicPic.classList.toggle("wiggle")
-    let random = Math.floor(Math.random()*answers.length)
-  console.log(random)
-  console.log(answers)
-alert(answers[random])
-  },2000)
-
+    setTimeout(() => {
+        magicPic.classList.toggle("wiggle")
+        random = Math.floor(Math.random() * answers.length)
+        console.log(random)
+        console.log(answers)
+        func()
+    }, 2000)
 })
